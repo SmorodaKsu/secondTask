4a2eea83d789259eafef84056048381c328ec128
ksu <ksunoonnn@yandex.ru>
2025-02-20 18:52:17 +0300

    коммит

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         int initialBalance = 100; // начальный баланс счета
-        int deposit = 3080; // сумма пополнения
+        int deposit = 1100; // сумма пополнения
 
-        int bonus = (deposit > 1000) ? deposit / 10 : 0;
+        int bonus = (deposit > 1000) ? deposit / 100 : 0;
 
         // итог
         int finalBalance = initialBalance + deposit + bonus;
